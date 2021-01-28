ab77e7c8bc54add0e82b48a70363b8467a7093ff
erickLuizA <erickluiz69@gmail.com>
2021-01-28 17:10:14 -0300

    navArgs and toolbar with back button

--- app/src/main/java/com/deverick/marvelousheroes/ui/fragments/DetailsFragment.kt
+++ app/src/main/java/com/deverick/marvelousheroes/ui/fragments/DetailsFragment.kt
@@ -6,17 +6,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import com.deverick.marvelousheroes.R
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import androidx.navigation.ui.setupWithNavController
 import com.deverick.marvelousheroes.viewmodels.DetailsViewModel
+import com.deverick.marvelousheroes.databinding.DetailsFragmentBinding
 
 class DetailsFragment : Fragment() {
 
     private val viewModel: DetailsViewModel by viewModels()
+    private lateinit var binding: DetailsFragmentBinding
+    private val args: DetailsFragmentArgs by navArgs()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.details_fragment, container, false)
+    ): View {
+        binding = DetailsFragmentBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupToolbar()
+
+        binding.characterName.text = args.character.name
+    }
+
+    private fun setupToolbar() {
+        val navController = findNavController()
+
+        binding.detailsToolbar.setupWithNavController(navController)
+        binding.detailsToolbar.title = null
     }
 }
