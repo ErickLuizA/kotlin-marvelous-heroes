0be28fcf9d75d8d9be7139cdaee34b59ffd2e881
erickLuizA <erickluiz69@gmail.com>
2021-02-01 23:55:02 -0300

    favorite fragment toolbar and recycler view

--- app/src/main/java/com/deverick/marvelousheroes/ui/fragments/FavoritesFragment.kt
+++ app/src/main/java/com/deverick/marvelousheroes/ui/fragments/FavoritesFragment.kt
@@ -6,20 +6,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.deverick.marvelousheroes.viewmodels.FavoritesViewModel
 import com.deverick.marvelousheroes.databinding.FavoritesFragmentBinding
+import com.deverick.marvelousheroes.ui.adapters.FavoritesAdapter
 
 class FavoritesFragment : Fragment() {
 
     private lateinit var binding: FavoritesFragmentBinding
+    private lateinit var favoritesAdapter: FavoritesAdapter
     private val viewModel: FavoritesViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FavoritesFragmentBinding.inflate(layoutInflater)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupToolbar()
+        setupRecyclerView()
+    }
+
+    private fun setupToolbar() {
+        val navController = findNavController()
+
+        binding.detailsToolbar.setupWithNavController(navController)
+        binding.detailsToolbar.title = null
+    }
+
+    private fun setupRecyclerView() {
+        favoritesAdapter = FavoritesAdapter()
+
+        binding.rvFavorites.apply {
+            adapter = favoritesAdapter
+            layoutManager = GridLayoutManager(activity, 2)
+        }
     }
 }
